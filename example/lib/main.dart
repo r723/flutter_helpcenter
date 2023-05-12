5bca2daf27add7a02d19354717fd11f82e3692e1
rahulV <variyarahul6677@gmail.com>
2023-05-12 11:56:50 +0530

    remove credential

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,11 +40,11 @@ class _MyAppState extends State<MyApp> {
               ),
               onPressed: () {
                 ZendeskHelpcenter.initialize(
-                    appId: "a14de18f781d1bd2c0660152083275f2c43b31ea1b30e2b3",
-                    clientId: "mobile_sdk_client_0114e6e9c12d9b12967c",
+                    appId: "Your zendesk app id from zendesk developer console",
+                    clientId: "Your zendesk client id from zendesk developer console",
                     nameIdentifier:
-                        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImlYanNxeGRYdGVSOW5MZGFfOGJ4ciJ9.eyJpc3MiOiJodHRwczovL2xvb3Btb25leS1kZXYuZXUuYXV0aDAuY29tLyIsInN1YiI6InNtc3w2MzRkMWJlNTBmYTBkNjBhNzA2OWRmNDUiLCJhdWQiOiJodHRwczovL2xvb3Btb25leS1kZXYuZXUuYXV0aDAuY29tL2FwaS92Mi8iLCJpYXQiOjE2ODM1MjMzNTksImV4cCI6MTY4MzYwOTc1OCwiYXpwIjoia1pxZ0QzVjhidDZmQWlDY3huWDZWZUhTSkM3Q1lSUm4iLCJzY29wZSI6InJlYWQ6Y3VycmVudF91c2VyIHVwZGF0ZTpjdXJyZW50X3VzZXJfbWV0YWRhdGEgZGVsZXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBjcmVhdGU6Y3VycmVudF91c2VyX21ldGFkYXRhIGNyZWF0ZTpjdXJyZW50X3VzZXJfZGV2aWNlX2NyZWRlbnRpYWxzIGRlbGV0ZTpjdXJyZW50X3VzZXJfZGV2aWNlX2NyZWRlbnRpYWxzIHVwZGF0ZTpjdXJyZW50X3VzZXJfaWRlbnRpdGllcyBvZmZsaW5lX2FjY2VzcyIsImd0eSI6WyJyZWZyZXNoX3Rva2VuIiwicGFzc3dvcmQiXX0.uziBGyjVJpivuNrajx0ykKT62TUS1hRjqKb2lFMJOLD8fz6A2aboDVw9EfROHsQA5En-MawADzo048ZmAnrEmtliDrXJWPOfu-WjbbgzKs984CcxqTNS8kQ-N5WDSLpcy3DkfB4ZLKTU_uboUrgzG6Sqm-ZqLEwyQp0sY4Je1Elpr7N4Zkh_g1Svmf7T5q-8P0TWRQBxBybTzTk4j-UcubJlgzLaxCzm1TZIG_BX5TAczXpur-3sk0qhfWCrw4xKKSJxkoVG869qkjSMsvwg0cdh0c7_fINJ6bfQfEYnIDeDQB3E9Dce9ZFtC1CuXbDxXRJ5NNkSzGlYd_FE4b7ADw",
-                    urlString: "https://loopmoney.zendesk.com");
+                        "Unique id for user , you can put jwt token or access token",
+                    urlString: "App url from of zendesk app");
               },
               child: const Text('Initialize'),
             ),
