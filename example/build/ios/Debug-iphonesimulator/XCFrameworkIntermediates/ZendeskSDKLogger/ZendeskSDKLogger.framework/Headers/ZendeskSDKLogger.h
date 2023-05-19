0386405bb790e19883279ca58f8de036bc236c0b
rahulvariya <variyarahul6677@gmail.com>
2023-05-19 16:23:12 +0530

    fixes

---
+++ example/build/ios/Debug-iphonesimulator/XCFrameworkIntermediates/ZendeskSDKLogger/ZendeskSDKLogger.framework/Headers/ZendeskSDKLogger.h
@@ -0,0 +1,26 @@
+//
+// ZendeskSDKLogger.h
+// Logger
+//
+// Created by zendesk on 22/06/2020.
+//
+// Copyright © 2020 zendesk. All rights reserved.
+//
+// By downloading or using the Zendesk Mobile SDK, You agree to the Zendesk Master
+// Subscription Agreement https://www.zendesk.com/company/customers-partners/#master-subscription-agreement
+// and Application Developer and API License
+//
+// Agreement https://www.zendesk.com/company/customers-partners/#application-developer-api-license-agreement and
+// acknowledge that such terms govern Your use of and access to the Mobile SDK.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZendeskSDKLogger/PublicHeader.h>
+
+
---
+++ example/build/ios/Debug-iphonesimulator/XCFrameworkIntermediates/ZendeskSDKStorage/ZendeskSDKStorage.framework/Headers/ZendeskSDKStorage.h
@@ -0,0 +1,26 @@
+//
+// ZendeskSDKStorage.h
+// Storage
+//
+// Created by zendesk on 04/06/2020.
+//
+// Copyright © 2020 zendesk. All rights reserved.
+//
+// By downloading or using the Zendesk Mobile SDK, You agree to the Zendesk Master
+// Subscription Agreement https://www.zendesk.com/company/customers-partners/#master-subscription-agreement
+// and Application Developer and API License
+//
+// Agreement https://www.zendesk.com/company/customers-partners/#application-developer-api-license-agreement and
+// acknowledge that such terms govern Your use of and access to the Mobile SDK.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Storage.
+FOUNDATION_EXPORT double StorageVersionNumber;
+
+//! Project version string for Storage.
+FOUNDATION_EXPORT const unsigned char StorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZendeskSDKStorage/PublicHeader.h>
+
+
